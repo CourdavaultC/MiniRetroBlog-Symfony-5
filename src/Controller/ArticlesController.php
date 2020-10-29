668486a3134f478a403130fe6ec078860f85e6a9
Courdavault C <courdavault.christophe@yahoo.fr>
2020-10-29 21:59:54 +0100

    First commit for this blog make with symfony

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Articles;
+use App\Entity\Images;
+use App\Form\ArticlesType;
+use App\Repository\ArticlesRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/articles")
+ */
+class ArticlesController extends AbstractController
+{
+    /**
+     * @Route("/", name="articles_index", methods={"GET"})
+     */
+    public function index(ArticlesRepository $articlesRepository): Response
+    {
+        return $this->render('articles/index.html.twig', [
+            'articles' => $articlesRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="articles_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $article = new Articles();
+        $form = $this->createForm(ArticlesType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // On récupère les images transmises
+            $images =$form->get('images')->getData();
+
+            // On boucle sur les images
+            foreach($images as $image){
+                // On génère un nouveau nom de fichier
+                $fichier = md5(uniqid()).'.'.$image->guessExtension();
+
+                // On copie le fichier dans le dossier upload
+                $image->move(
+                    $this->getParameter('images_directory'),
+                    $fichier
+                );
+                // On stocke l'image dans la base de donnée
+                $img = new Images();
+                $img->setName($fichier);
+                $article->addImage($img);
+            }
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('articles_index');
+        }
+
+        return $this->render('articles/new.html.twig', [
+            'article' => $article,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="articles_show", methods={"GET"})
+     */
+    public function show(Articles $article): Response
+    {
+        return $this->render('articles/show.html.twig', [
+            'article' => $article,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="articles_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Articles $article): Response
+    {
+        $form = $this->createForm(ArticlesType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+             // On récupère les images transmises
+             $images =$form->get('images')->getData();
+
+             // On boucle sur les images
+             foreach($images as $image){
+                 // On génère un nouveau nom de fichier
+                 $fichier = md5(uniqid()).'.'.$image->guessExtension();
+ 
+                 // On copie le fichier dans le dossier upload
+                 $image->move(
+                     $this->getParameter('images_directory'),
+                     $fichier
+                 );
+                 // On stocke l'image dans la base de donnée
+                 $img = new Images();
+                 $img->setName($fichier);
+                 $article->addImage($img);
+             }
+ 
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('articles_index');
+        }
+
+        return $this->render('articles/edit.html.twig', [
+            'article' => $article,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="articles_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Articles $article): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$article->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($article);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('articles_index');
+    }
+
+    /**
+     * @Route("/supprime/image/{id}", name="articles_delete_image", methods={"DELETE"})
+     */
+    public function deleteImage(Images $image, Request $request)
+    {
+        $data = json_decode($request->getContent(), true);
+
+        // On vérifie si le token est valide
+        if($this->isCsrfTokenValid('delete'.$image->getId(), $data['_token'])){
+            // On récupère le nom de l'image
+            $nom = $image->getName();
+            // On supprime le fichier
+            unlink($this->getParameter('images_directory').'/'.$nom);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($image);
+            $em->flush();
+
+            // On répond en Json
+            return new JsonResponse(['success' => 1]);
+        }else{
+            return new JsonResponse(['error' => 'Token invalide'], 400);
+        }
+    }
+}
